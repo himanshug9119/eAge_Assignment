f17558daaf116fc5817d14cf7552345ca05dc5bd
Himanshu Gupta <guptasitapur489@gmail.com>
2024-03-27 19:16:35 +0530

    Updated Home.jsx

--- assignment/src/pages/Home.jsx
+++ assignment/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../components/home.css";
-import Header from "../components/Header";
+import Header from "../components/header";
 const Home = () => {
   return (
     <>
