565ccc35eeead676e5acd1f73ecb25a7d5434279
Himanshu Gupta <guptasitapur489@gmail.com>
2024-03-29 10:35:11 +0530

    Updated browserrouter with hashrouter in app.jsx

--- assignment/src/App.jsx
+++ assignment/src/App.jsx
@@ -1,21 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Assignment1 from "./pages/assignment1";
 import Assignment2 from "./pages/assignment2";
 import Home from "./pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import "font-awesome/css/font-awesome.min.css";
+
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/assignment1" element={<Assignment1 />} />
-          <Route path="/assignment2" element={<Assignment2 />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/assignment1" element={<Assignment1 />} />
+        <Route path="/assignment2" element={<Assignment2 />} />
+      </Routes>
+    </Router>
   );
 }
 
